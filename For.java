fe6f83d558aa5c7a34aaa6d8038c20cb38511fc4
SaitovN <nuriksaitov03@gmailcom>
2023-06-02 16:38:52 +0600

    Create For.java
    For

---
+++ For.java
@@ -0,0 +1,11 @@
+package academy.devonline.java.basic.section01_setup.section05_setup;
+
+public class For {
+    public static void main(String[] args) {
+        System.out.println("Before for");
+        for (int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+        System.out.println("After for");
+    }
+}
